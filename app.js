f09d8d871709c243f490fdbd2fbc50187a75b291
Melissa Du <melissa.du2@gmail.com>
2016-04-09 08:49:43 -0700

    styling for user landing page/login page

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ var sass = require('node-sass-middleware');
 var multer = require('multer');
 var upload = multer({ dest: path.join(__dirname, 'uploads') });
 
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  *
